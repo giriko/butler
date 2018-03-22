58118ed1bdd2d816fc5b27da49255a587b654a3c
Kadence B <thebeckken@gmail.com>
2018-03-22 14:53:58 -0500

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject butler "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [fast-zip "0.7.0"]]
+  :main ^:skip-aot butler.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/butler/core.clj
@@ -0,0 +1,10 @@
+(ns butler.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn -copy
+  "I Copy Files" [&dest, old])
