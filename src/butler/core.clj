f0530a727a468010be499d1fd44c4108a3dacf9f
Kadence B <thebeckken@gmail.com>
2018-03-27 21:25:12 -0500

    Add some logic to manage profiles
    need to look up how -> actually works
    (added test/ dir because it should be, eventually.)

--- src/butler/core.clj
+++ src/butler/core.clj
@@ -1,6 +1,8 @@
 (ns butler.core
   (:gen-class))
 
+(def empty-profile '())
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -8,3 +10,24 @@
 
 (defn -copy
   "I Copy Files" [&dest, old])
+
+(defstruct new-profile :profile :directory)
+
+
+(defn -save-profiles [profile filename]
+  (spit filename (pr-str profile)))
+
+(defn add-profile [pro & new-pro]
+  (into pro new-pro))
+
+(defn -load-profiles [filename]
+  (read-string
+   (slurp filename)))
+
+(defn -new-profile-list []
+  (-save-profiles empty-profile "profiles"))
+
+(defn make-profile [name dir]
+  (-> -load-profiles ("profiles")
+      (add-profile (struct new-profile name dir))
+      (-save-profiles "profiles")))
